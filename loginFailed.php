cb2c9e5ba050d29b4ee416b5e28b39384e084781
Katon Bingham <katonbingham@gmail.com>
2020-11-08 18:38:08 -0800

    Updated CSS

--- loginFailed.php
+++ loginFailed.php
@@ -1,4 +1,12 @@
-<?php
-echo "Login has failed, invalid email or password.<br>";
-?>
-<a href="/CloudTestApp/signin.html">Return to sign in<a>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<br>
+<div class="mx-auto" style="width: 200px;">
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Login Failed</h5>
+            <p class="card-text">You have entered an incorrect email or password.</p>
+            <a href="/CloudTestApp/signin.html" class="btn btn-primary">Return to Sign In</a>
+        </div>
+    </div>
+</div>
+
--- loginSuccess.php
+++ loginSuccess.php
@@ -1,6 +1,14 @@
-<?php
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+<br>
+<div class="mx-auto" style="width: 200px;">
+    <div class="card" style="width: 18rem;">
+        <div class="card-body">
+            <h5 class="card-title">Login Successful!</h5>
+            <p class="card-text">Welcome.</p>
+<!--            <a href="/CloudTestApp/signin.html" class="btn btn-primary">Return to Sign In</a>-->
+        </div>
+    </div>
+</div>
 
-echo "You are logged in.<br>";
 
-?>
-<a href="/CloudTestApp/index.html">Return to main page<a>
+<!--<a href="/CloudTestApp/index.html">Return to main page<a>-->
